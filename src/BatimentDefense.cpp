0bf1c01d4beaafb787fb4f6e24eceef9c0065f15
NASR <ahmed-nasrallah.bouheddadj@etu.univ-lyon1.fr>
2023-03-07 08:31:23 +0100

    Update BatimentDefense

---
+++ src/BatimentDefense.cpp
@@ -0,0 +1,23 @@
+    /**
+    @brief Description brève du fichier BatimentDefense.h
+    Description détaillée du fichier BatimentDefense.h
+    @author P2111020/P2018147/P2105542
+    @file BatimentDefense.h
+    @version 1.0
+    @date 07/03/2023
+    */
+
+    #ifndef BATIMENTDEFENSE_H
+    #define BATIMENTDEFENSE_H
+
+    /**
+    @brief Classe représentant les propriétés d'un batiment de défense
+    */
+    class BatimentDefense
+    {
+        public:
+
+        private:
+
+    };
+    #endif
---
+++ src/BatimentDefense.h
@@ -0,0 +1,3 @@
+#include "BatimentDefense.h"
+#include <iostream>
+
