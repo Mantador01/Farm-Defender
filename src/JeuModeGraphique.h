fcc9c9ac5d6074014e68afe90c812bf4cf4ede04
BOUHEDDADJ AHMED NASRALLAH p2111020 <ahmed-nasrallah.bouheddadj@etu.univ-lyon1.fr>
2023-04-04 06:39:07 +0000

    Replace JeuModeGraphique.h

--- src/JeuModeGraphique.h
+++ src/JeuModeGraphique.h
@@ -10,59 +10,14 @@
     #ifndef JEUMODEGRAPHIQUE_H
     #define JEUMODEGRAPHIQUE_H
 
-    #include "Jeu.h"
-
-    #include <SDL2/SDL.h>
-    #include <SDL2/SDL_ttf.h>
-    #include <SDL2/SDL_image.h>
-    #include <SDL2/SDL_mixer.h>
-
-    class Image {
-
-    private:
-
-        SDL_Surface * m_surface;
-        SDL_Texture * m_texture;
-        bool m_hasChanged;
-
-    public:
-        Image () ;
-        ~Image();
-        void loadFromFile (const char* filename, SDL_Renderer * renderer);
-        void loadFromCurrentSurface (SDL_Renderer * renderer);
-        void draw (SDL_Renderer * renderer, int x, int y, int w=-1, int h=-1);
-        SDL_Texture * getTexture() const;
-        void setSurface(SDL_Surface * surf);
-};
-
     /**
     @brief Classe résponsable de l'affichage
     */
     class JeuModeGraphique
     {
         public:
-            JeuModeGraphique ();
-            ~JeuModeGraphique ();
-            void sdlBoucle ();
-            void sdlAff ();
-
+        
         private:
-            Jeu jeu;
 
-            SDL_Window * window;
-            SDL_Renderer * renderer;
-
-            TTF_Font * font;
-            Image font_im;
-            SDL_Color font_color;
-
-            Mix_Chunk * sound;
-            bool withSound;
-
-            Image im_zombi;
-            Image im_plaine;
-
-            bool souris;
-            bool touche;
     };
     #endif
