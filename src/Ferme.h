4d8a7004ec17f5bcbe4266109e2a5437c7c1ef4c
NASR <ahmed-nasrallah.bouheddadj@etu.univ-lyon1.fr>
2023-05-01 14:38:36 +0200

    docu2

--- src/Ferme.h
+++ src/Ferme.h
@@ -1,5 +1,11 @@
-
-
+/**
+@brief Description brève du fichier Ferme.h
+Description détaillée du fichier Ferme.h
+@author Ahmed/Alexandre/Sara
+@file Image.h
+@version 1.0
+@date 01/05/2023
+*/
 
 #ifndef FERME_H
 #define FERME_H
@@ -9,41 +15,60 @@
 
 using namespace std;
 
+
+
+
+/**
+@brief Classe représentant une ferme
+*/
 class Ferme {
 public:
 
-    
-    Ferme(StockageRessources& stockage);
+/**
+@brief Constructeur de la classe Ferme
+@param stockage Stockage de ressources associé à la ferme
+*/
+Ferme(StockageRessources& stockage);
 
-    
-    
-    void production(bool est_vivant);
+/**
+@brief Produit des ressources en fonction du statut de vie de la ferme
+@param est_vivant Statut de vie de la ferme
+*/
+void production(bool est_vivant);
 
-    
-    void degat(int degat);
+/**
+@brief Enlève des points de vie à la ferme
+@param degat Nombre de points de vie à enlever
+*/
+void degat(int degat);
 
-    
-   bool est_vivant();
-   
-   
-   void creation(Vect2 posi);
+/**
+@brief Obtient le statut de vie de la ferme
+@return true si la ferme est en vie, false sinon
+*/
+bool est_vivant();
 
-   
-        StockageRessources* m_stockage;
+/**
+@brief Crée une nouvelle ferme à la position spécifiée
+@param posi Position de la nouvelle ferme
+*/
+void creation(Vect2 posi);
 
-    
-   Vect2 get_position();
+StockageRessources* m_stockage; /**< Stockage de ressources associé à la ferme */
+
+/**
+@brief Obtient la position de la ferme
+@return Position de la ferme
+*/
+Vect2 get_position();
 
 
 private:
 
-    
+int m_pointsDeVie; /**< Nombre de points de vie de la ferme */
+Vect2 pos; /**< Position de la ferme */
 
-    
-    int m_pointsDeVie;
 
-    
-    Vect2 pos;
 };
 
 #endif // FERME_H
