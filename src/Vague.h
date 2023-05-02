3a86712220bbd28a15edcdb7023cd314d5f743ec
NASR <ahmed-nasrallah.bouheddadj@etu.univ-lyon1.fr>
2023-05-02 09:59:22 +0200

    DOCUMENTATION

--- src/Vague.h
+++ src/Vague.h
@@ -1,4 +1,11 @@
-
+    /**
+    @brief Description brève du fichier Vague.h
+    Description détaillée du fichier Vague.h
+    @author Ahmed/Alexandre/Sara
+    @file Vague.h
+    @version 1.0
+    @date 01/05/2023
+    */ 
 
 
 #ifndef VAGUE_H
@@ -9,15 +16,27 @@
 #include <queue>
 #include "Ennemi.h"
 
+
+/**
+@brief Classe représentant une vague d'ennemis
+*/
 class Vague {
 public:
-    Vague();
-    Ennemi getNextEnnemi();
+
+/**
+@brief Constructeur par défaut: Initialise une vague vide sans ennemis
+*/
+Vague();
+
+/**
+@brief Fonction qui renvoie le prochain ennemi de la vague
+@return Un ennemi de la queue d'ennemis
+*/
+Ennemi getNextEnnemi();
+
 
 private:
-    std::queue<Ennemi> m_ennemis;
+std::queue<Ennemi> m_ennemis; /**< Queue contenant les ennemis de la vague */
 };
 
-
-
-#endif 
+#endif
